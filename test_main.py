85c4762e5aceb3b7ad567136bbc232e9bbfe3bc3
jknees <cortezcardinal@gmail.com>
2016-12-08 14:19:35 -0800

    Fixed error with tests

--- test_main.py
+++ test_main.py
@@ -13,7 +13,7 @@ def test_interpret_time():
 def test_interpret_date():
 	assert flask_main.interpret_date("11/11/2016") == '2016-11-11T00:00:00-08:00'
 
-def test_test_insert_database():
+def test_insert_database():
 	global _id
 	begin_date = arrow.get(flask_main.interpret_date('12/04/2016'))
 	end_date = arrow.get(flask_main.interpret_date('12/13/2016'))
@@ -21,6 +21,6 @@ def test_test_insert_database():
 	end_time = arrow.get(flask_main.interpret_time('10:00 pm')).datetime.timetz()
 
 	flask_main.insertToDatabase(['2016.12.11 10:00 22:00 | free time'], _id, str(begin_date), str(end_date), str(begin_time), str(end_time))
-	data = flask_main.collection.find_one({'uuid': uuid})
+	data = flask_main.collection.find_one({'uuid': _id})
 	assert data['_id'] == _id
 	assert data['events'] == '2016.12.11 10:00 22:00 | free time'
