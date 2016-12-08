796cb9bd6ea73342bfaaf3c99a1d46ad548e075e
jknees <cortezcardinal@gmail.com>
2016-12-08 14:11:19 -0800

    Hopefully fixed spacing error

--- test_main.py
+++ test_main.py
@@ -8,16 +8,16 @@ import bson
 _id = str(uuid.uuid4())
 
 def test_interpret_time():
- 	assert flask_main.interpret_time("10:00 am") == '2016-01-01T10:00:00-08:00'
+	assert flask_main.interpret_time("10:00 am") == '2016-01-01T10:00:00-08:00'
 
 def test_interpret_date():
- 	assert flask_main.interpret_date("11/11/2016") == '2016-11-11T00:00:00-08:00'
+	assert flask_main.interpret_date("11/11/2016") == '2016-11-11T00:00:00-08:00'
 
 def test_test_insert_database(_id):
 	begin_date = arrow.get(flask_main.interpret_date('12/04/2016'))
-  	end_date = arrow.get(flask_main.interpret_date('12/13/2016'))
-  	begin_time = arrow.get(flask_main.interpret_time('10:00 am')).datetime.timetz()
-  	end_time = arrow.get(flask_main.interpret_time('10:00 pm')).datetime.timetz()
+	end_date = arrow.get(flask_main.interpret_date('12/13/2016'))
+	begin_time = arrow.get(flask_main.interpret_time('10:00 am')).datetime.timetz()
+	end_time = arrow.get(flask_main.interpret_time('10:00 pm')).datetime.timetz()
 
 	flask_main.insertToDatabase(['2016.12.11 10:00 22:00 | free time'], _id, begin_date, end_date, begin_time, end_time)
 	data = collection.find_one({'uuid': uuid})
